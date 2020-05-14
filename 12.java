539d80c9ee8abbb8ac501a48a43c12d1ab0157af
Chetan Paliwal <chetanpaliwal22@gmail.com>
2020-05-14 11:20:31 -0400

    Update 12.java O(Log(n))

--- 12.java
+++ 12.java
@@ -1,20 +1,20 @@
 class Solution {
     public int singleNonDuplicate(int[] nums) {
-        int len = nums.length;
-        HashMap<Integer,Integer> hm = new HashMap<Integer,Integer>();
+        int low = 0;
+        int high = nums.length-1;
+        if(high == 1){
+            return nums[low];
+        }
         
-        for(int i=0;i<len;i++){
-            if(hm.containsKey(nums[i])){
-              hm.remove(nums[i]);
-            }    
-            else{
-              hm.put(nums[i],1);
+        while(low<high){
+            int mid = (low+high)/2;
+        
+            if(((mid%2) == 0 && nums[mid] == nums[mid+1]) || ((mid%2) == 1 && nums[mid] == nums[mid-1])){
+                low = mid+1;
+            }else {
+                high = mid;
             }
         }
-        
-        for(Integer key: hm.keySet()){
-          return key;
-        }
-        return -1;
+        return nums[high];
     }
 }
